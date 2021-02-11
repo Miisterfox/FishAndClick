429c488d9588f43fd059d1ccc641218949144953
Joogny <statitixx@gmail.com>
2021-02-11 09:49:27 +0100

    Euh j'ai tout mis

---
+++ app/src/main/java/com/example/fishnclick/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.fishnclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int clicks = 0;
+    //loldqsd
+    ImageButton FishClick;
+    TextView FishView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void FishClick(View view) {
+        clicks++;
+        FishView.setText(clicks+ " clicks!");
+    }
+
+}
