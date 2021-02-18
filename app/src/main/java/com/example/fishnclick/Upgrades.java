f25fa53330581e7a24882aab45609c0ec8352491
Misterfox <64079721+Miisterfox@users.noreply.github.com>
2021-02-18 10:45:32 +0100

    Menu

--- app/src/main/java/com/example/fishnclick/Upgrades.java
+++ app/src/main/java/com/example/fishnclick/Upgrades.java
@@ -6,41 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-<<<<<<< Updated upstream
-=======
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
->>>>>>> Stashed changes
-public class MainActivity extends AppCompatActivity {
-<<<<<<< Updated upstream
-    private int clicks = 0;
-    TextView FishView;
-=======
-    private static TextView FishView = (TextView) findViewById(R.id.FishView);
-    private static Fish fish1 = new Fish("bar",1);
->>>>>>> Stashed changes
+public class Upgrades extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FishView = (TextView) findViewById(R.id.FishView);
-<<<<<<< Updated upstream
-=======
+        setContentView(R.layout.activity_upgrades);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setSelectedItemId(R.id.fish);
+        bottomNav.setSelectedItemId(R.id.upgrades);
         bottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
                     case R.id.upgrades:
-                        startActivity(new Intent(getApplicationContext(),Upgrades.class));
                         break;
                     case R.id.fish:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         break;
                     case R.id.shop:
                         startActivity(new Intent(getApplicationContext(),Shop.class));
@@ -48,13 +33,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
->>>>>>> Stashed changes
     }
-
-
-    public void FishClick(View view) {
-        clicks++;
-        FishView.setText(clicks+ " clicks!");
-    }
-
 }
