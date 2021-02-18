98a7636971de0b2981cf65cb54a0f699ef7fa749
Joogny <statitixx@gmail.com>
2021-02-18 10:04:15 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/fishnclick/MainActivity.java
+++ app/src/main/java/com/example/fishnclick/MainActivity.java
@@ -6,20 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-    //commentaire lol
 public class MainActivity extends AppCompatActivity {
-    private TextView FishView = (TextView) findViewById(R.id.FishView);;
-    private static Fish fish1 = new Fish("bar",1);
+    private TextView FishView;
+    private static Fish fish1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fish1 = new Fish("bar",1);
+        FishView = (TextView) findViewById(R.id.FishView);
+
     }
 
 
     public void FishClick(View view) {
         fish1.addClick();
-        FishView.setText(fish1.getClicks()+ " clicks!");
+        FishView.setText(fish1.getClicks() + " clicks!");
     }
 
 }
