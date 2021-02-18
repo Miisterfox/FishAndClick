1a6e19cd05407a09b40afc9c00518d6ba68d1bb3
Misterfox <64079721+Miisterfox@users.noreply.github.com>
2021-02-18 10:05:56 +0100

    New activity

--- app/src/main/java/com/example/fishnclick/MainActivity.java
+++ app/src/main/java/com/example/fishnclick/MainActivity.java
@@ -8,8 +8,13 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+<<<<<<< Updated upstream
     private int clicks = 0;
     TextView FishView;
+=======
+    private static TextView FishView = (TextView) findViewById(R.id.FishView);
+    private static Fish fish1 = new Fish("bar",1);
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
