88301df6e752362113bf0bd6b2a416640b496e92
Misterfox <64079721+Miisterfox@users.noreply.github.com>
2021-03-03 21:56:08 +0100

    Revert "Revert "!""
    This reverts commit e18e176340a7a102f1a5ed7438f56a9cc27126a8.

---
+++ app/src/main/java/com/example/fishnclick/UpgradesFragment.java
@@ -0,0 +1,36 @@
+package com.example.fishnclick;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class UpgradesFragment extends Fragment {
+
+    public UpgradesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upgrades, container, false);
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.Buy:
+                Fish fish = MainActivity.getFish("bar");
+                FishFragment.setMoney(-(FishFragment.getMoney()*fish.getLevel()));
+                fish.levelUp();
+                break;
+        }
+    }
+
+}
