cd0073921cc7f1756db32e3de6cdd10e735ecd8c
Joogny <statitixx@gmail.com>
2021-03-03 13:01:41 +0100

    z

--- app/src/main/java/com/example/fishnclick/MainActivity.java
+++ app/src/main/java/com/example/fishnclick/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.fishnclick;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+<<<<<<< Updated upstream
+=======
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+>>>>>>> Stashed changes
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,11 +21,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
+<<<<<<< Updated upstream
 public class MainActivity extends AppCompatActivity {
 
+=======
+public class MainActivity extends AppCompatActivity{
+    Fragment fishFragment;
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
 
     }
