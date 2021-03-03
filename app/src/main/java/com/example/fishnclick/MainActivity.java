c70763ff5ecccde4fd5f0f8e1582564b49c123b0
Joogny <statitixx@gmail.com>
2021-03-03 23:58:43 +0100

    dq

--- app/src/main/java/com/example/fishnclick/MainActivity.java
+++ app/src/main/java/com/example/fishnclick/MainActivity.java
@@ -1,18 +1,11 @@
 package com.example.fishnclick;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.os.Handler;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
